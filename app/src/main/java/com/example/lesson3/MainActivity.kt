1a42cd0a84f12331cd02241de5e7924450f71362
Netanelsh054 <netanelsh054@gmail.com>
2024-12-02 23:17:15 +0200

    Build some views

--- app/src/main/java/com/example/lesson3/MainActivity.kt
+++ app/src/main/java/com/example/lesson3/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lesson3
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addStudentButton = findViewById<Button>(R.id.btnMainAddStudent)
-        }
+//        val addStudentButton = findViewById<Button>(R.id.btnMainAddStudent)
+        val addStudentButton: Button = findViewById(R.id.btnMainAddStudent)
+
+        //option 1
+//        class ButtonOnClickListener : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                val intent = Intent(this@MainActivity, AddStudentActivity::class.java)
+//            }
+//
+//        }
+
+        //option 2
+        //val listener = ButtonOnClickListener()
+        //addStudentButton.setOnClickListener(ButtonOnClickListener())
+
+        //option 3
+        //addStudentButton.setOnClickListener(View.OnClickListener { })
+
+        //lambda expression
+        //addStudentButton.setOnClickListener {}
+
+        addStudentButton.setOnClickListener(::onAddStudentButtonClicked)
     }
+    fun onAddStudentButtonClicked(view: View) {
+        val intent = Intent(this, AddStudentActivity::class.java)
+        startActivity(intent)
+    }
+}
